cf05e444c10be0dec085852a226b28c73d9c4ef2
Kelp Green <gerard.labelle+github@gmail.com>
2014-11-18 10:21:59 -0800

    Ch. 2, pp. 16-17, added static HTML pages.

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 'use strict';
 
 var http = require('http'),
+    fs   = require('fs'),
     port = process.env.PORT || 5000;
 
+var serveStaticFile = function (res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+
+    fs.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500 - Internal Error');
+        }
+        else {
+            res.writeHead(responseCode, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+};
+
 http.createServer(function (req, res) {
-	// Normalize URL by removing query-string, optional trailing slash, and
-	// making it lowercase.
-	var path = req.url
-		.replace(/\/?(?:\?.*)?$/, '')
-		.toLowerCase();
+    // Normalize URL by removing query-string, optional trailing slash, and
+    // making it lowercase.
+    var path = req.url
+        .replace(/\/?(?:\?.*)?$/, '')
+        .toLowerCase();
 
-	switch(path) {
-		case '':
-			res.writeHead(200, { 'Content-Type': 'text/plain' });
-			res.end('Homepage');
-			break;
+    switch(path) {
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html');
+            break;
 
-		case '/about':
-			res.writeHead(200, { 'Content-Type': 'text/plain' });
-			res.end('About');
-			break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
 
-		default:
-			res.writeHead(404, { 'Content-Type': 'text/plain' });
-			res.end('Not Found');
-			break;
-	}
+        case '/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png');
+            break;
+
+        default:
+            serveStaticFile(res, '/public/notfound.html', 'text/html', 404);
+            break;
+    }
 }).listen(port);
 
 console.log('Server started on localhost:%d', port);
